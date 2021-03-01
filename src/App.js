2ee084d99626045ae97a13fdabc4e049adfe9779
vinod kumar yada <vinnuvinod33@gmail.com>
2021-03-01 21:30:29 +0530

    Added new changes

--- src/App.js
+++ src/App.js
@@ -5,18 +5,15 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
+        
+        
+        <h1          className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
-        </a>
+          Learn React i'm vinod
+        </h1>
       </header>
     </div>
   );
